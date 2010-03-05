8d8a6a684bedc0ec5c9aeccf0f4365a76e9f3c13
aquilax <aquilax@55980f74-2899-11df-8ece-253d35979a0a>
2010-03-05 21:16:17 +0000

    Choose characrter started

--- system/application/views/home/start_tpl.php
+++ system/application/views/home/start_tpl.php
@@ -2,11 +2,16 @@
   print_r($characters);
   if($characters){
     echo '<table>';
+    echo '<tr>';
+    echo '<th>'.lang('Name').'</th>';
+    echo '<th>'.lang('Class').'</th>';
+    echo '<th>'.lang('Level').'</th>';
+    echo '</tr>';
     foreach($characters as $character){
       echo '<tr>';
-      echo '<td>'.$character['name'].'</td>';
+      echo '<td>'.anchor('home/start/'.$character['id'], $character['name']).'</td>';
       echo '<td>'.$character['classname'].'</td>';
-      echo '<td>'.$character['classname'].'</td>';
+      echo '<td>'.$character['level'].'</td>';
       echo '</tr>';
     }
     echo '</table>';
